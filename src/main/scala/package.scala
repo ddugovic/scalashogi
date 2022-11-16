94d0de5e898b5e30cf03205735ddd9ef5ae88440
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-11-16 07:46:47 -0600

    Remove unused toMove conversions

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -18,24 +18,9 @@ package object shogi extends scalalib.Common with scalalib.OrnicarOption with sc
   type ParsedMoves   = List[format.ParsedMove]
 
   // TODO: remove backward compatibility code
-  import cats.data.Validated
-  import shogi.format.{ CsaMove, KifMove, ParsedDrop, ParsedMove }
   import shogi.format.usi.Usi
-  implicit def toMove(usi: Usi, situation: Situation): Move = usi match {
-    case m: Usi.Move => m(situation)
-    case d: Usi.Drop => d(situation)
-  }
-  implicit def toMove(situation: Situation, usi: Usi): Move = toMove(usi, situation)
   implicit def toUsi(move: Move): Usi = move match {
     case m: PieceMove => Usi.Move(m.orig, m.dest, m.promotion)
     case d: PieceDrop => Usi.Drop(d.role, d.pos)
   }
-  implicit def toMove(parsedMove: ParsedMove, situation: Situation): Validated[String, Move] =
-    parsedMove match {
-      case c: CsaMove    => c.toMove(situation)
-      case k: KifMove    => k.toMove(situation)
-      case d: ParsedDrop => d.toMove(situation)
-    }
-  implicit def toMove(situation: Situation, parsedMove: ParsedMove): Validated[String, Move] =
-    toMove(parsedMove, situation)
 }
